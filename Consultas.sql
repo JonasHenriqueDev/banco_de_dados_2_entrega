986626c6bf8b0b743f460f6caca1f544b0f2a8de
Pedro H. Mota <110642021+mota-pedro@users.noreply.github.com>
2023-09-10 19:56:41 -0300

    Consulta dos últimos requisitos

--- Consultas.sql
+++ Consultas.sql
@@ -26,18 +26,12 @@ SELECT * FROM public.requisicao WHERE usuario_id = 3 AND observacao = 'Observaç
 SELECT * FROM public.requisicao WHERE usuario_id = 3 AND curso_id = 12;
 
 --RF 017 - Visualizar indicadores sobre as requisições enviadas
-SELECT 
-    r.status_requisicao,
-    COUNT(*) AS quantidade,
-    SUM(CASE WHEN a.eixo = 'ENSINO' THEN a.ch_por_certificado ELSE 0 END) AS horas_ensino,
-    SUM(CASE WHEN a.eixo = 'PESQUISA' THEN a.ch_por_certificado ELSE 0 END) AS horas_pesquisa,
-    SUM(CASE WHEN a.eixo = 'EXTENSAO' THEN a.ch_por_certificado ELSE 0 END) AS horas_extensao,
-    SUM(CASE WHEN a.eixo = 'GESTAO' THEN a.ch_por_certificado ELSE 0 END) AS horas_gestao
-FROM public.requisicao AS r
-JOIN public.certificado AS c ON r.id = c.requisicao_id
-JOIN public.atividade AS a ON c.atividade_id = a.id
-WHERE r.usuario_id = 311
-GROUP BY r.status_requisicao;
+-- Visualizar status das requisções
+SELECT status_requisicao, id FROM public.requisicao WHERE usuario_id = 1;
+-- Visualizar quantidade de requisições com status 'RASCUNHO
+SELECT COUNT(status_requisicao) FROM public.requisicao WHERE usuario_id = 1 AND status_requisicao = 'RASCUNHO';
+-- Visualizar quantidade de horas contabilizadas por eixo
+SELECT horas_ensino, horas_extensao, horas_gestao, horas_pesquisa FROM public.usuario WHERE id = 1;
 
 --RF 018 - Criar rascunhos de requisições
 -- Aqui nós só colocamos o status da requisição como RASCUNHO
@@ -53,10 +47,70 @@ ALTER TABLE public.certificado DROP CONSTRAINT fk4let25y9w1sqkslmiqk40njxi;
 ALTER TABLE public.certificado
   ADD CONSTRAINT fk4let25y9w1sqkslmiqk40njxi
   FOREIGN KEY (requisicao_id)
-  REFERENCES public.requisicao(id) 
+  REFERENCES public.requisicao(id)
   ON DELETE CASCADE;
 
 -- Realizar a querry
-SELECT * FROM public.requisicao WHERE status_requisicao = 'ACEITO';
+SELECT * FROM public.requisicao WHERE status_requisicao = 'RASCUNHO';
 DELETE FROM public.requisicao
 WHERE status_requisicao = 'RASCUNHO' AND usuario_id = 3 AND id = 853;
+
+--RF 020 - Alterar rascunhos de requisições
+-- Aqui nós fizemos outro INSERT, pois o registro havia sido apagado
+
+
+--RF 021 - Enviar solicitação à coordenação
+-- Aqui alteramos o status da requisição de 'RASCUNHO' para 'TRANSITO'
+SELECT * FROM public.requisicao WHERE usuario_id = 3 AND id = 1320;
+UPDATE public.requisicao
+SET status_requisicao = 'TRANSITO'
+WHERE status_requisicao = 'RASCUNHO' AND usuario_id = 3 AND id = 1320;
+
+--RF 022
+
+
+--RF 023 - Visualizar dados do discente
+-- Aqui criamos um View que contém o somatório de horas de um usuário específico
+CREATE VIEW total_horas AS SELECT horas_ensino + horas_extensao + horas_gestao + horas_pesquisa FROM public.usuario WHERE id = 1;
+SELECT * FROM total_horas;
+
+--RF 024 - Visualizar fluxo de requisição
+-- Aqui utilizamos SELECT para verificar o status de uma requisição específica
+SELECT status_requisicao FROM public.requisicao WHERE usuario_id = 1 AND id = 861;
+
+--RF 026 - Visualização de Perfil
+-- Utilizamos o SELECT para verificar apenas o nome do usuário e o email, pois não há campo que represente uma foto de perfil
+SELECT nome_completo, email FROM public.usuario WHERE id = 24;
+
+--RF 027 - Arquivar Solicitação
+-- Utilizamos o UPDATE para atualizar os registros não arquivados onde o status está como 'NEGADO'
+UPDATE public.requisicao
+SET arquivada = 'true'
+WHERE usuario_id = 102 AND arquivada = 'false' AND status_requisicao = 'NEGADO';
+
+--RF 028 - Deletar Solicitação
+-- Criamos uma nova tabela lixeira
+CREATE TABLE lixeira (
+    id BIGINT PRIMARY KEY,
+    arquivada BOOLEAN,
+    criacao DATE,
+    data_de_submissao DATE,
+    id_requisicao VARCHAR(255),
+    observacao TEXT,
+    requisicao_arquivo_assinada BYTEA,
+    status_requisicao VARCHAR(255),
+    token VARCHAR(255),
+    curso_id BIGINT,
+    usuario_id BIGINT
+);
+
+-- Adicionamos na tabela lixeira as requisições de um usuário onde o status estava como 'NEGADO' ou 'PROBLEMA'
+INSERT INTO public.lixeira SELECT * FROM public.requisicao WHERE usuario_id = 114 AND status_requisicao = 'NEGADO' OR status_requisicao = 'PROBLEMA';
+-- Utilizamos o DELETE para deletar esses registros da tabela requisicao
+DELETE FROM public.requisicao
+WHERE usuario_id = 114 AND status_requisicao = 'NEGADO' OR status_requisicao = 'PROBLEMA';
+
+--RF 030 - Modal de Lixeira
+-- Utilizamos um SELECT para visualizar as requisições na lixeira de um usuário
+SELECT * FROM public.lixeira WHERE usuario_id = 114;
+
